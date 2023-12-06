7fc3917041d754fa4bf581e9d4d15558590ad324
Brian Inca <brianincah10@gmail.com>
2023-12-06 11:14:08 -0500

    Lenguaje y plantillas - Semana 08

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,15 +4,33 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-
+    estudiantes = [ 'Alexia Asuncion', 
+                    'Axel Barzola',
+                    'Gerson Sahuma',
+                    'Carlos tu Dios']
+    return render(request,'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web Con DJango',
+        'estudiantes': estudiantes
+    })
 
 def ventana1(request):
-    return render(request, 'ventana1.html')
+    return render(request,'ventana1.html',{
+        'titulo':'Saludo',
+        'autor_saludo':'PhD. Brian Inca'
+    })
 
 
 def ventana2(request):
-    return render(request, 'ventana2.html')
+    a = 10
+    b = 20
+    rango_numeros = range(a,b+1)
+    return render(request,'ventana2.html',{
+        'titulo':'Rango',
+        'a':a,
+        'b':b,
+        'rango_numeros':rango_numeros
+    })
 
 
 def ventana3(request):
